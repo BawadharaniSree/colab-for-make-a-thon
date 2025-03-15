21cc5e184d4b69320c99f62a89deaea8e4338d1b
BawadharaniSree <152498388+BawadharaniSree@users.noreply.github.com>
2025-03-16 01:07:26 +0530

    Update layout.tsx tab logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+            <link rel="icon" href="/new tab logo.png" type="image/png" />
       <body>
         {children}
         <CustomCursor />
